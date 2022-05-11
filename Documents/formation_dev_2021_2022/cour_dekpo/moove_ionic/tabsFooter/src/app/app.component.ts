d1d4dbf85851cceca613c65a72fa6bd240fb378e
BabyDice <secure.contain.protect10888@gmail.com>
2022-05-11 19:20:18 +0200

    first build for android studio with command : ionic build /  ionic capacitor add android /

--- Documents/formation_dev_2021_2022/cour_dekpo/moove_ionic/tabsFooter/src/app/app.component.ts
+++ Documents/formation_dev_2021_2022/cour_dekpo/moove_ionic/tabsFooter/src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Inbox', url: '/side-menu/Inbox', icon: 'mail' },
     { title: 'Homekbox', url: '/tabs/tab1', icon: 'home' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Outbox', url: '/side-menu/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/side-menu/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/side-menu/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/side-menu/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/side-menu/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   
